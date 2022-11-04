36820b273d3def787035303f3ae82b9e4b7153e8
itzewis <fotos3647@gmail.com>
2022-11-04 19:16:58 -0400

    04/10 ta com um bug

--- app/src/main/java/com/example/manoaplicativo/formaPagamento.kt
+++ app/src/main/java/com/example/manoaplicativo/formaPagamento.kt
@@ -1,5 +1,6 @@
 package com.example.manoaplicativo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -10,6 +11,30 @@ class formaPagamento : AppCompatActivity() {
         setContentView(R.layout.activity_forma_pagamento)
 
         var debito = findViewById<TextView>(R.id.cartaoDebito)
+        var credito = findViewById<TextView>(R.id.cartaoCredito)
+        var pix = findViewById<TextView>(R.id.pix)
+
+
+        debito.setOnClickListener {
+
+            intent = Intent(this,pagamento_Debito::class.java)
+            startActivity(intent)
+
+        }
+
+        credito.setOnClickListener {
+
+            intent = Intent(this,pagamento_Credito::class.java)
+            startActivity(intent)
+
+        }
+
+        pix.setOnClickListener {
+
+            intent = Intent(this,pagamento_Pix::class.java)
+            startActivity(intent)
+
+        }
 
 
     }
